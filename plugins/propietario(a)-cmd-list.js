86347a280e9e913d704b7b0fec508512519e1398
Henry <henry.arcangello@proton.me>
2023-11-29 18:43:07 +0000

    update plugins/propietario(a)-cmd-list.js

--- plugins/propietario(a)-cmd-list.js
+++ plugins/propietario(a)-cmd-list.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-conn.reply(m.chat, `ღ 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊/𝙏𝙀𝙓𝙏𝙊𝙎 𝘼𝙎𝙄𝙂𝙉𝘼𝘿𝙊𝙎
+conn.reply(m.chat, `ღ 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊
 
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*ღ 𝘾𝙊𝘿𝙄𝙂𝙊:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*ღ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊/𝙏𝙀𝙓𝙏𝙊𝙎* ${value.text}`).join('\n\n')}
 `.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
