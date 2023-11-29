e261fb35c0130988147a4d2e7d00a03a0be5fff8
Henry <henry.arcangello@proton.me>
2023-11-29 19:11:22 +0000

    update plugins/propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -7,7 +7,7 @@ await delay(1000 * 1);
 await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key});
 await delay(1000 * 1);
 await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key});
-await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 | 𝙍𝙚𝙨𝙩𝙖𝙧𝙩`, edit: key});
+await conn.sendMessage(m.chat, {text: `𝙍𝙚𝙞𝙣𝙞𝙘𝙞𝙖𝙧 `, edit: key});
     process.send('reset')
   } else throw 'eh'
 }
