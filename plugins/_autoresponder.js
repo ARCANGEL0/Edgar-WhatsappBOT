c9cf43f6665f5026dca978be68bdf64254f98779
Henry <henry.arcangello@proton.me>
2023-11-29 07:21:47 +0000

    update plugins/_autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -3,11 +3,11 @@ let handler = m => m
 
 handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
-    
+/*    
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-let stiker = await sticker(imagen1, false, global.packname, global.author)  
+let stiker = await sticker(imagen2, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝓔𝓭𝓰𝓪𝓻 𝓑𝓞𝓣 🐈‍⬛  🐈', body: '𝓔𝓭𝓰𝓪𝓻 𝓑𝓞𝓣 🐈‍⬛ ', sourceUrl: `https://github.com/ARCANGEL0/Edgar-WhatsappBOT`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝓔𝓭𝓰𝓪𝓻 𝓑𝓞𝓣 🐈‍⬛  🐈', body: '𝓔𝓭𝓰𝓪𝓻 𝓑𝓞𝓣 🐈‍⬛ ', sourceUrl: `https://github.com/ARCANGEL0/Edgar-WhatsappBOT`, thumbnail: imagen2}}})}*/
     
 return !0 }
 export default handler
