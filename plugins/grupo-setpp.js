98f7e7e2411430290f83028e57978bd6f0ed3bdc
Henry <henry.arcangello@proton.me>
2023-11-29 00:24:52 +0000

    update plugins/grupo-setpp.js

--- plugins/grupo-setpp.js
+++ plugins/grupo-setpp.js
@@ -3,9 +3,9 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let img = await q.download()
-if (!img) throw `${lenguajeGB['smsAvisoMG']()}️𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉`
-await conn.updateProfilePicture(m.chat, img).then(_ => m.reply(`${lenguajeGB['smsAvisoEG']()}𝙎𝙀 𝘾𝘼𝙈𝘽𝙄𝙊 𝙇𝘼 𝙁𝙊𝙏𝙊 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊 𝘾𝙊𝙉 𝙀́𝙓𝙄𝙏𝙊`))
-} else throw `${lenguajeGB['smsAvisoMG']()}️𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝙀 𝘼 𝙐𝙉𝘼 𝙄𝙈𝘼𝙂𝙀𝙉`}
+if (!img) throw `${lenguajeGB['smsAvisoMG']()}️𝘼𝙣𝙚𝙭𝙚 𝙪𝙢𝙖 𝙞𝙢𝙖𝙜𝙚𝙢`
+await conn.updateProfilePicture(m.chat, img).then(_ => m.reply(`${lenguajeGB['smsAvisoEG']()}𝗙𝗼𝘁𝗼 𝗱𝗼 𝗴𝗿𝘂𝗽𝗼 𝗮𝗹𝘁𝗲𝗿𝗮𝗱𝗮 𝗰𝗼𝗺 𝘀𝘂𝗰𝗲𝘀𝘀𝗼`))
+} else throw `${lenguajeGB['smsAvisoMG']()}️𝘼𝙣𝙚𝙭𝙚 𝙪𝙢𝙖 𝙞𝙢𝙖𝙜𝙚𝙢`}
 handler.command = /^setpp(group|grup|gc)?$/i
 handler.group = true
 handler.admin = true
