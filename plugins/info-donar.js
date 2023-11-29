70fc7434cd0a44e3fc32d99ed64fa1faace1b985
Henry <henry.arcangello@proton.me>
2023-11-29 00:51:51 +0000

    update plugins/info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -31,6 +31,6 @@ mentionedJid:[m.sender],
 ['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}*/
-handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i
+handler.command = /^dona(te|si)|doar|apoyar|paypal|donating$/i
 handler.exp = 80
 export default handler
