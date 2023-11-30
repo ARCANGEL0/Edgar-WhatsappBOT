9057b123d703ee13a924ba95cb0b1f71f9d72d4e
Henry <henry.arcangello@proton.me>
2023-11-30 00:45:54 +0000

    update plugins/propietario(a)-update.js

--- plugins/propietario(a)-update.js
+++ plugins/propietario(a)-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 //} catch {
-var update = execSync('git remote set-url origin https://github.com/ARCANGEL0/Edgar-WhatsappBOT.git && git pull origin master')
+var update = execSync('git remote set-url origin https://github.com/ARCANGEL0/Edgar-WhatsappBOT.git && git pull origin master -f')
 await m.reply(update.toString())
 } catch {
 await m.reply(`${fg}`) 
