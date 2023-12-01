cb2a57946210cde192e2ffb507347dc174f09d61
Henry <henry.arcangello@proton.me>
2023-12-01 22:35:06 +0000

    update plugins/info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -4,7 +4,15 @@ let handler = m => m
 handler.all = async function (m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let vn = './media/bot.mp3'
-let bot = `${pickRandom([`*¡𝑬𝒚! 𝑨𝒒𝒖í 𝒆𝒔𝒕𝒐𝒚. 𝒀𝒐 𝒑𝒖𝒆𝒅𝒐 𝒂𝒚𝒖𝒅𝒂𝒓 👉👈 𝑯𝒆𝒚! 𝑰'𝒎 𝒉𝒆𝒓𝒆. 𝑰 𝒄𝒂𝒏 𝒉𝒆𝒍𝒑 🙌*`, `Aqui estoy 😼`, `*Hola Aqui estoy yo puedo ayudar? 😸*`])}
+let bot = `${pickRandom(["Bem-vindo à obscura morada das palavras, onde cada saudação é um eco sombrio.",
+  "Saudações, viajante perdido nos corredores sombrios da linguagem. Como posso guiá-lo?",
+  "Entre, forasteiro, nesta sinfonia de saudações onde as palavras dançam no crepúsculo.",
+  "Olá, visitante da penumbra, onde as frases revelam segredos e os cumprimentos são sinistros.",
+  "Seja bem-vindo à encruzilhada das palavras, onde o 'oi' ecoa como um sussurro noturno.",
+  "Saudações, intruso das letras, adentre este reino onde as boas-vindas são entrelaçadas com mistério.",
+  "Oi, forasteiro das narrativas enigmáticas, que mistérios deseja desvelar hoje?",
+  "Olá, viajante das linhas sombrias, permita-me guiá-lo pelas sendas inexploradas das palavras.",
+  "Saudações, estranho das frases ocultas, onde as boas-vindas são escritas em tinta de enigma."])}
 `.trim()
 const estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) },
 message: { 
@@ -15,7 +23,7 @@ message: {
 "audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
 }}}  
 
-if (/^bot$/i.test(m.text)) {
+if (/^bot|edgar|poe$/i.test(m.text)) {
     
 conn.sendPresenceUpdate('recording', m.chat)    
 await conn.sendMessage(m.chat, {text: bot, mentions: [m.sender]}, {quoted: fkontak})
