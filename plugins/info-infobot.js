818620c7f6e39e8be026ee081a1b84437599632e
Henry <henry.arcangello@proton.me>
2023-12-01 16:29:24 +0000

    update plugins/info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -40,8 +40,8 @@ const cpu = cpus.reduce((last, cpu, _, { length }) => {
   })
 const { restrict } = global.db.data.settings[conn.user.jid] || {}
 const { autoread } = global.opts
-let pp = './media/menus/Menu1.jpg'
-let vn = './media/infobot.mp3'
+let pp = '../media/menus/Menu1.jpg'
+let vn = '../media/infobot.mp3'
 let enlace = { contextInfo: { externalAdReply: {title: wm + ' ', body: 'Instagram' , sourceUrl: ig, thumbnail: await(await fetch(pp)).buffer() }}}
 
 let dos = enlace
