84b9a83b1fedaca3a06049fe858b35cbfe8b0417
Henry <henry.arcangello@proton.me>
2023-12-02 07:41:31 +0000

    update plugins/info-velocidad.js

--- plugins/info-velocidad.js
+++ plugins/info-velocidad.js
@@ -91,7 +91,7 @@ ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type =>
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `
 console.log(caption)
-await conn.sendFile(m.chat, "teste", 'gata.jpg', imagen2, fkontak)
+await conn.sendFile(m.chat, "teste", 'gata.jpg', caption, fkontak)
 //await conn.sendButton(m.chat, wm, caption, gata.getRandom(), [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, dos.getRandom())
 } catch (e) {
   console.log(caption)
