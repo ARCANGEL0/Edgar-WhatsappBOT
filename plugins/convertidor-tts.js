7be8cba48b3be7f2e6a5a313e3de2544bcc0ee4e
Henry <henry.arcangello@proton.me>
2023-12-03 20:29:04 +0000

    update plugins/convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,59 +1,57 @@
-// Import 'say' library
-import say from 'say';
-import { readFileSync, unlinkSync } from 'fs';
-import { join } from 'path';
-import OpenAI from "openai";
 
-const openai = new OpenAI();
+import gtts from 'node-gtts'
+// import 'say' from say
+import { readFileSync, unlinkSync } from 'fs'
+import { join } from 'path'
 
-const speechFile = path.resolve("tts.mp3")
-
-const defaultLang = 'pt-br';
-
+const defaultLang = 'pt-br'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    let lang = args[0];
-    let text = args.slice(1).join(' ');
 
-    if ((args[0] || '').length !== 2) {
-        lang = defaultLang;
-        text = args.join(' ');
-    }
-
-    if (!text && m.quoted?.text) text = m.quoted.text;
-
-    let res;
-    
-    try {
-        res = await tts(text, lang);
-    } catch (e) {
-        m.reply(e + '');
-        text = args.join(' ');
-
-        if (!text) {
-            throw `${lenguajeGB['smsAvisoMG']()}╭━━━━━━━━━⬣
-`    }
-
-        await conn.sendPresenceUpdate('recording', m.chat);
-        res = await tts(text, lang);
-    } finally {
-        if (res) conn.sendFile(m.chat, speechFile, 'tts.mp3', null, m, true);
-    }
-};
-
-handler.help = ['tts <lang> <teks>'];
-handler.tags = ['tools'];
-handler.command = /^g?tts|totts$/i;
-
-export default handler;
-
-function tts(text, lang = 'pt-br'){
-  
-const mp3 = await openai.audio.speech.create({
-    model: "tts-1",
-    voice: "onyx",
-    input: text
-  });
-  console.log(speechFile);
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(speechFile, buffer);
+let lang = args[0]
+let text = args.slice(1).join(' ')
+if ((args[0] || '').length !== 2) {
+lang = defaultLang
+text = args.join(' ')
 }
+if (!text && m.quoted?.text) text = m.quoted.text
+
+let res
+try { res = await tts(text,lang) }
+catch (e) {
+m.reply(e + '')
+text = args.join(' ')
+if (!text) throw `${lenguajeGB['smsAvisoMG']()}╭━━━━━━━━━⬣
+┃
+┃ 🥀🪦 𝐌𝐞 𝐬𝐮𝐬𝐬𝐮𝐫𝐫𝐞 𝐮𝐦 𝐬𝐞𝐠𝐫𝐞𝐝𝐨
+┃ 𝐧𝐚𝐬 𝐬𝐨𝐦𝐛𝐫𝐚𝐬 𝐞 𝐥𝐡𝐞 𝐫𝐞𝐭𝐨𝐫𝐧𝐚𝐫𝐞𝐢
+┃ 𝐚 𝐯𝐨𝐳 𝐝𝐨 𝐥𝐚𝐦𝐞𝐧𝐭𝐨 𝐧𝐚 𝐟𝐨𝐫𝐦𝐚 𝐝𝐞
+┃ 𝐮𝐦 𝐚𝐮𝐝𝐢𝐨 𝐦𝐚𝐥𝐝𝐢𝐭𝐨
+┃
+┃ 𝙀𝙓𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} ptbr Tudo
+┃ que amei, amei sozinho
+┃
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ 
+┃ 𝓔𝓭𝓰𝓪𝓻 𝓐𝓵𝓵𝓪𝓷 𝓑𝓸𝓽 🐈‍⬛ | ${vs}
+╰━━━━━━━━━━━━━━━━━━⬣
+*`
+await conn.sendPresenceUpdate('recording', m.chat)
+res = await tts(text,lang)
+} finally {
+if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+}}
+handler.help = ['tts <lang> <teks>']
+handler.tags = ['tools']
+handler.command = /^g?tts|totts$/i
+export default handler
+function tts(text, lang = 'pt-br') {
+console.log(lang, text)
+return new Promise((resolve, reject) => {
+try {
+let tts = gtts(lang)
+let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
+tts.save(filePath, text, () => {
+resolve(readFileSync(filePath))
+unlinkSync(filePath)
+})
+} catch (e) { reject(e) }
+})}
