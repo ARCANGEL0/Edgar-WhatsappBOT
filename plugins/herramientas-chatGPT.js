17c863d250d65fdb48037105377c568d597f80d6
Henry <henry.arcangello@proton.me>
2023-12-03 23:33:10 +0000

    update plugins/herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -45,10 +45,10 @@ conn.sendPresenceUpdate('typing', m.chat);
 async function getOpenAIChatCompletion(texto) {
 const openaiAPIKey = global.openai_key;
 let chgptdb = global.chatgpt.data.users[m.sender];
-chgptdb.push({ role: sistema1, content: texto });
+chgptdb.push({ role: texto, content: texto });
 const url = "https://api.openai.com/v1/chat/completions";
 const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": sistema1, "content": sistema1 }, ...chgptdb, ]};
+const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": texto, "content": texto }, ...chgptdb, ]};
 const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
 const result = await response.json();
 const finalResponse = result.choices[0].message.content;
