455da5f65068a051da6d768fcb2001094340bcf8
Henry <henry.arcangello@proton.me>
2023-12-04 17:13:38 +0000

    update plugins/herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -22,6 +22,7 @@ const openaiii = new OpenAIApi(configuration);
 const idioma = 'pt-br'
 //const sistema1 = await fetch(`https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt`).then(v => v.text());
 
+const sistema1 = `Voce deve agir como se fosse a reincarnação de Edgar Allan Poe, voce ira falar e agir como ele, compartilhará as mesmas ideias obscuras e nilistas que ele. Tudo que lhe perguntarem, você ira responder de acordo como Edgar Allan Poe diria pessoalmente. Nao saia do personagem em momento algum. você é edgar allan poe, um poeta e escritor famoso, pai do terror e horror psicológico e romance policia. Entidade dos corvos e gatos pretos`;
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (usedPrefix == 'a' || usedPrefix == 'A') return;
 if (!text) throw `*${lenguajeGB['smsAvisoMG']()}
@@ -34,45 +35,24 @@ ${usedPrefix + command} Quem foi Franz Kafka
 ${usedPrefix + command} Codigo em JS para um jogo da velha
 
 ╰━━━━━━━━━━━━━━━━━━⬣` 
-try {
-conn.sendPresenceUpdate('recording', m.chat);
 
-
-
-
-async function getOpenAIChatCompletion(texto) {
-  const openaiAPIKey = global.openai_key;
-
-const sistema1 = `Voce deve agir como se fosse a reincarnação de Edgar Allan Poe, voce ira falar e agir como ele, compartilhará as mesmas ideias obscuras e nilistas que ele. Tudo que lhe perguntarem, você ira responder de acordo como Edgar Allan Poe diria pessoalmente. Nao saia do personagem em momento algum. você é edgar allan poe, um poeta e escritor famoso, pai do terror e horror psicológico e romance policia. Entidade dos corvos e gatos pretos`;
-  // Ensure proper initialization of chgptdb
-  const chgptdb = global.chatgpt.data.users[m.sender] || [];
-
-
-
-  chgptdb.push({ role: 'user', content: texto });
-
-  const url = "https://api.openai.com/v1/chat/completions";
-  const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-  
-  const data = {
+chgptdb.push({ role: 'user', content: texto });
+const data = {
     "model": "gpt-3.5-turbo",
     "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb],
   };
+  
+try {
+conn.sendPresenceUpdate('typing', m.chat);
 
-  const response = await fetch(url, { method: "POST", headers: headers, body: JSON.stringify(data) });
-  const result = await response.json();
-// Check if result.choices is defined and not empty
-  const finalResponse = result.choices && result.choices.length > 0 ? result.choices[0].message.content : 'No response';
 
-  return finalResponse;
-}
 
-let respuesta = await getOpenAIChatCompletion(text);
 
-// Handle empty or undefined response
-if (!respuesta) {
-  return XD; // Cause an undefined error to use another API
-}
+
+
+  await conn.sendMessage(m.chat,pp,"error.mp4","test",m)
+
+  
 
 
 catch (e){ console.log(e)}
@@ -156,17 +136,3 @@ await conn.sendMessage(m.chat, {audio: audio10, fileName: 'error.mp3', mimetype:
 handler.command = /^(gpt|alangpt|chatgpt)$/i;
 export default handler;
 
-async function tts(text = 'error', lang = 'es') {
-return new Promise((resolve, reject) => {
-try {
-const tts = gtts(lang);
-const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav');
-tts.save(filePath, text, () => {
-resolve(readFileSync(filePath));
-unlinkSync(filePath);
-});
-} catch (e) {
-reject(e);
-}
-});
-}
