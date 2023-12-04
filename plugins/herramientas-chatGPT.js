6d48eb4d7681df61bcdfdcbad4a588d7015e93ec
Henry <henry.arcangello@proton.me>
2023-12-04 15:17:12 +0000

    update plugins/herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -74,7 +74,7 @@ await conn.sendFile(m.chat, pp, 'gata.mp4', respostagpt,m)
 catch(e){
   console.log(e)
     await conn.sendFile(m.chat, pp, 'gata.mp4', 
-    global.openai_key,m)
+    openai_key,m)
 }
 
 }
