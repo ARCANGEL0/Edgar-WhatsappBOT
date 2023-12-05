1b8b25f4c68141df3d43dc47600ac980de10b2a4
Henry <henry.arcangello@proton.me>
2023-12-05 19:33:58 +0000

    update plugins/afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -11,7 +11,9 @@ if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
 user.afk = + new Date
 user.afkReason = text
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}*⬣━━━「 AFK ⚰️」━━━⬣*
-${lenguajeGB['smsAfkM1A']()} *@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1B']()}${text ? '\n ' + text : ''}`, m, { mentions: [m.sender] })
+*🕯️💀𝐍𝐚𝐨 𝐜𝐡𝐚𝐦𝐞* *@${m.sender.split("@")[0]}*  *「 𝐈𝐫𝐚 𝐬𝐞 𝐚𝐮𝐬𝐞𝐧𝐭𝐚𝐫 𝐛𝐫𝐞𝐯𝐞𝐦𝐞𝐧𝐭𝐞 𝐩𝐚𝐫𝐚 𝐟𝐢𝐧𝐬 𝐩𝐚𝐫𝐭𝐢𝐜𝐮𝐥𝐚𝐫𝐞𝐬」*
+  
+  *𝐂𝐚𝐮𝐬𝐚:*${text ? '\n ' + text : ''}`, m, { mentions: [m.sender] })
 }
 
 handler.command = /^afk$/i
