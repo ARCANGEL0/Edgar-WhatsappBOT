381b1dac3bf9eb3d579351e89f1c38f214a10f17
Henry <henry.arcangello@proton.me>
2023-12-05 14:05:35 +0000

    update plugins/grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -7,7 +7,7 @@ conn.reply(m.chat, lenguajeGB.smsSetB(), fkontak, m)
 } else throw `${lenguajeGB['smsSetB2']()}`
 }
 handler.command = ['setbye', 'despedida'] 
-handler.botAdmin = true
+handler.botAdmin = false
 handler.admin = true
 handler.group = true
 export default handler
