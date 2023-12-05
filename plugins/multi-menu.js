249b7bc06085332d46320e4c36f9e7cfc785d249
Henry <henry.arcangello@proton.me>
2023-12-05 01:14:12 +0000

    update plugins/multi-menu.js

--- plugins/multi-menu.js
+++ plugins/multi-menu.js
@@ -88,7 +88,7 @@ let menuh = `╭━━━━━━━━━⬣
 
 await m.reply(menuh)
 } 
-} catch (e) {
+ catch (e) {
 await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
 console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
 console.log(e)}
