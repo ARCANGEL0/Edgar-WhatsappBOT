e77f87b0a74c73337c3910203523d2e3805bdd59
Henry <henry.arcangello@proton.me>
2023-12-05 14:03:54 +0000

    Delete the file at 'plugins/propietario(a)-autoadmin.js'

--- plugins/propietario(a)-autoadmin.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `*���𝘼𝘿𝙈𝙄𝙉 𝘿𝙀 𝙂𝙍𝙐𝙋𝙊 *`
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^admin.|atad|autoadmin$/i
-handler.rowner = true
-handler.botAdmin = true
-export default handler
