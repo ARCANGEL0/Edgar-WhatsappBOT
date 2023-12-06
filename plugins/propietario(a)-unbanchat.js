1643aa45f95458f1ef677664fe158e906d6139b8
Henry <henry.arcangello@proton.me>
2023-12-06 07:50:41 +0000

    update plugins/propietario(a)-unbanchat.js

--- plugins/propietario(a)-unbanchat.js
+++ plugins/propietario(a)-unbanchat.js
@@ -21,7 +21,7 @@ global.db.data.chats[m.chat].isBanned = false
 }
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
-handler.command =  /^(abrirbot|boton|ativarbot)$/i
+handler.command =  /^(abrirbot|ligarbot|ativarbot)$/i
 handler.botAdmin = false
 handler.admin = true
 export default handler;
