93468f99f9414114873db989f5bb70539ca0c731
Henry <henry.arcangello@proton.me>
2023-12-07 01:34:46 +0000

    update plugins/grupo-destraba.js

--- plugins/grupo-destraba.js
+++ plugins/grupo-destraba.js
@@ -15,7 +15,7 @@ if(args[0] && args[1]) {
 const formatoHora = 'HH:mm';
 
 const horarioLembrete = moment(horario, formatoHora).tz('America/Sao_Paulo');
-  const timeout = horarioLembrete - horaatual;
+  const timeout = horario - horaatual;
   console.log("hora atual    "+horaatual)
   console.log("hora definida    "+horario)
   console.log(timeout)
