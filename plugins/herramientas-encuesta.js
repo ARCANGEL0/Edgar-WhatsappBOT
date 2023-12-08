3e714bb161c7185a61bcca3df125195dc0c56bd7
Henry <henry.arcangello@proton.me>
2023-12-08 22:30:58 +0000

    update plugins/herramientas-encuesta.js

--- plugins/herramientas-encuesta.js
+++ plugins/herramientas-encuesta.js
@@ -1,23 +1,19 @@
-let handler = async (m, { conn, text, args, participants, usedPrefix, command }) => {	
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+	
+if (!args[0]) throw `✳️ Missing text for survey \n\n📌 Example : \n*${usedPrefix + command}* Message  |GURU|xd`
+if (!text.includes('|')) throw  `✳️ Separate surveys with *|* \n\n📌 Example : \n*${usedPrefix + command}* my survey|n  |GURU|xd|piro`
+
+let name = await conn.getName(m.sender)
 let a = []
 let b = text.split('|')
-try{
-if (!b[0]) return conn.reply(m.chat, `${mg}𝙐𝙎𝙀 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙁𝙊𝙍𝙈𝘼\n*${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....*\n\n𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝘼𝙎 𝙁𝙊𝙇𝙇𝙊𝙒𝙎\n*${usedPrefix + command} Survey Reason|text1|text2|text3....*`, fkontak, m) 
-if (!b[1]) return conn.reply(m.chat, `${mg}𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙊𝙋𝘾𝙄𝙊𝙉𝙀𝙎 𝙀𝙉 𝙇𝘼 𝙀𝙉𝘾𝙐𝙀𝙎𝙏𝘼 𝙐𝙎𝙀 *" | "*\n*${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....*\n\n𝙏𝙊 𝘾𝙍𝙀𝘼𝙏𝙀 𝙊𝙋𝙏𝙄𝙊𝙉𝙎 𝙄𝙉 𝙏𝙃𝙀 𝙎𝙐𝙍𝙑𝙀𝙔 𝙐𝙎𝙀 *" | "*\n*${usedPrefix + command} Survey Reason|text1|text2|text3....*`, fkontak, m)
-if (b[13]) return conn.reply(m.chat, `${fg}𝙈𝘼𝙓𝙄𝙈𝙊 *12* 𝙊𝙋𝘾𝙄𝙊𝙉𝙀𝙎!!\n\n𝙐𝙎𝙀 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙇𝘼 𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 𝙁𝙊𝙍𝙈𝘼\n*${usedPrefix + command} Motivo de Encuesta|texto1|texto2|texto3....*\n\n𝙈𝘼𝙓𝙄𝙈𝙐𝙈 *12* 𝙊𝙋𝙏𝙄𝙊𝙉𝙎!!\n\n𝙐𝙎𝙀 𝙏𝙃𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿 𝘼𝙎 𝙁𝙊𝙇𝙇𝙊𝙒𝙎\n*${usedPrefix + command} Survey Reason|text1|text2|text3....*`, fkontak, m)
-
-for (let c = 1; c < b.length; c++) { a.push([b[c]]) }
-let texto = `📊 𝙀𝙉𝘾𝙐𝙀𝙎𝙏𝘼 𝘾𝙍𝙀𝘼𝘿𝘼 𝙋𝙊𝙍\n*❤️⇢ ${conn.getName(m.sender)}*\n*${text.split('|')[0]}*`
-
-return conn.sendPoll(m.chat, texto, a, {mentions: m})}
-
-
-catch(e){
-  console.log(e)
+for (let c = 1 || 0; c < b.length; c++) {
+a.push([b[c]])
+			}
+			return conn.sendPoll(m.chat, `📊 *Survey requested by:* ${name}\n\n*Message:* ${text.split('|')[0]}`, a, m)
 }
-}
-handler.command = ['poll', 'enquete', 'crearencuesta', 'startpoll', 'encuestas', 'polls'] 
-handler.admin = true
+handler.help = ['poll <Guru|piro|xd>']
+handler.tags = ['group'] 
+handler.command = ['poll', 'encuesta', 'polling'] 
+handler.group = true
 
 export default handler
