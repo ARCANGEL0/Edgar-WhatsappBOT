e3609e9e89c7433f530c2d20b1d0f0c16608b6ea
Henry <henry.arcangello@proton.me>
2023-12-08 19:04:18 +0000

    update plugins/_chatgpt.js

--- plugins/_chatgpt.js
+++ plugins/_chatgpt.js
@@ -98,8 +98,8 @@ console.log(result.choices[0].message.content);
 
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
-let respuesta = await requestToChatGPT(m.text);
-return respuesta
+let respuesta = await requestToChatGPT(text);
+await m.reply(respuesta)
 await m.reply({ react: {
         text: "🌕", // use an empty string to remove the reaction
         key: m.key }
