e7d5508d34259fe6e5fe3061a2aa0754e46d3932
Henry <henry.arcangello@proton.me>
2023-12-08 22:34:11 +0000

    update plugins/herramientas-encuesta.js

--- plugins/herramientas-encuesta.js
+++ plugins/herramientas-encuesta.js
@@ -1,15 +1,12 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw `✳️ Missing text for survey \n\n📌 Example : \n*${usedPrefix + command}* Message  |GURU|xd`
-if (!text.includes('|')) throw  `✳️ Separate surveys with *|* \n\n📌 Example : \n*${usedPrefix + command}* my survey|n  |GURU|xd|piro`
-
-let name = await conn.getName(m.sender)
-let a = []
-let b = text.split('|')
-for (let c = 1 || 0; c < b.length; c++) {
-a.push([b[c]])
-			}
-			return conn.sendPoll(m.chat, `📊 *Survey requested by:* ${name}\n\n*Message:* ${text.split('|')[0]}`, a, m)
+	
+	try{
+	  conn.reply("porra")
+	}
+	catch(e){
+	  console.log(e)
+	}
 }
 handler.help = ['poll <Guru|piro|xd>']
 handler.tags = ['group'] 
