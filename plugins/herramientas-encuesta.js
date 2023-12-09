e252f25f12876afffa1791f87c6e2039b5a6bb5f
Henry <henry.arcangello@proton.me>
2023-12-09 11:07:18 +0000

    update plugins/herramientas-encuesta.js

--- plugins/herramientas-encuesta.js
+++ plugins/herramientas-encuesta.js
@@ -2,10 +2,11 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
 	await conn.reply(m.chat, "espera caralho ")
 	try{
+	  await conn.reply(m.chat, "foi porra ")
 	  conn.sendPoll(m.chat,"pica",["4cm,6cm,8cm,10cm"])
 	}
 	catch(e){
-	  console.log(e)
+	  await conn.reply(m.chat, e)
 	}
 }
 handler.help = ['poll <Guru|piro|xd>']
