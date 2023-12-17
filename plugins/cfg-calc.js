b2936c6edee11a8beecae03d2bf7cb9f6bf2aee4
Henry <henry.arcangello@proton.me>
2023-12-17 13:23:25 +0000

    update plugins/cfg-calc.js

--- plugins/cfg-calc.js
+++ plugins/cfg-calc.js
@@ -3,49 +3,45 @@ let handler = async (m, { conn, text }) => {
     global.quiz = global.quiz ? global.quiz : {}
     
     if (global.quiz.math) {
-        
-        
         m.reply('😨 𝙉𝙊 𝙃𝘼𝙂𝘼𝙎 𝙏𝙍𝘼𝙈𝙋𝘼!!\n𝘿𝙊 𝙉𝙊𝙏 𝘾𝙃𝙀𝘼𝙏!!');
-    }
-    else {
-    let val = text
-        .replace(/[^0-9\-\/+*×÷^πEe()!]/g, '')
-        .replace(/×/g, '*')
-        .replace(/÷/g, '/')
-        .replace(/\^|\*\*/g, '**')
-        .replace(/π|pi/gi, 'Math.PI')
-        .replace(/e/gi, 'Math.E')
-        .replace(/\//g, '/')
-        .replace(/(\d+)(!)/g, 'factorial($1)')
-        .replace(/\*×/g, '×');
-.replace(/(\d+)%(\d+)/g, '($1 % $2)/100') // Atualizado para calcular corretamente o módulo
-.replace(/(\d+\.\d+)/g, '($1)'); // Adicionado para reconhecer decimais   
-.replace(/(\d+\,\d+)/g, '($1)'); // Adicionado para reconhecer decimais   
-    let format = val
-        .replace(/Math\.PI/g, 'π')
-        .replace(/Math\.E/g, 'e')
-        .replace(/\//g, '÷')
-        .replace(/\*×/g, '×')
-        .replace(/!/g, '!')
-        .replace(/\*\*/g, '^');
-
-    try {
-        console.log(val);
-
-        let result = (new Function('factorial', 'return ' + val))(factorial);
+    } else {
+        let val = text
+            .replace(/[^0-9\-\/+*×÷^πEe()%!.,]/g, '') // Adicionado vírgula (,) para reconhecer decimais
+            .replace(/×/g, '*')
+            .replace(/÷/g, '/')
+            .replace(/\^|\*\*/g, '**')
+            .replace(/π|pi/gi, 'Math.PI')
+            .replace(/e/gi, 'Math.E')
+            .replace(/(\d+)(!)/g, 'factorial($1)')
+            .replace(/(\d+)%(\d+)/g, '($1 % $2)/100') // Atualizado para calcular corretamente o módulo
+            .replace(/(\d+\.\d+)/g, '($1)'); // Adicionado para reconhecer decimais
         
-        if (!result) throw result;
-        
-        m.reply(`*${text}* = _${result}_`);
-    } catch (e) {
-        if (e == undefined) {
-            throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙊𝙋𝙀𝙍𝘼𝘾𝙄𝙊𝙉 𝙈𝘼𝙏𝙀𝙈𝘼𝙏𝙄𝘾𝘼 𝙋𝘼𝙍𝘼 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙍 𝙀𝙇 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊/n/n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙈𝘼𝙏𝙇𝙃𝙀𝙈𝘼𝙏𝙄𝘾𝘼𝙇 𝙊𝙋𝙀𝙍𝘼𝙏𝙄𝙊𝙉 𝙏𝙊 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙏𝙀 𝙏𝙃𝙀 𝙍𝙀𝙎𝙐𝙇𝙏`;
+        let format = val
+            .replace(/Math\.PI/g, 'π')
+            .replace(/Math\.E/g, 'e')
+            .replace(/\//g, '÷')
+            .replace(/\*×/g, '×')
+            .replace(/!/g, '!')
+            .replace(/\*\*/g, '^');
+
+        try {
+            console.log(val);
+
+            let result = (new Function('factorial', 'return ' + val))(factorial);
+
+            if (!result) throw result;
+
+            m.reply(`*${text}* = _${result}_`);
+        } catch (e) {
+            if (e == undefined) {
+                throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙊𝙋𝙀𝙍𝘼𝘾𝙄𝙊𝙉 𝙈𝘼𝙏𝙀𝙈𝘼𝙏𝙄𝘾𝘼 𝙋𝘼𝙍𝘼 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙍 𝙀𝙇 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊/n/n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙈𝘼𝙏𝙇𝙇𝙀𝙈𝘼𝙏𝙄𝘾𝘼𝙇 𝙊𝙋𝙀𝙍𝘼𝙏𝙄𝙊𝙉 𝙏𝙊 𝘾𝘼𝙇𝘾𝙐𝙇𝘼𝙏𝙀 𝙏𝙃𝙀 𝙍𝙀𝙎𝙐𝙇𝙏`;
+            }
+
+            throw `${fg}𝙎𝙊𝙇𝙊 𝙎𝙀 𝘼𝘿𝙈𝙄𝙏𝙀𝙉 𝙉𝙐𝙈𝙀𝙍𝙊𝙎 𝙔 𝙎𝙄𝙈𝘽𝙊𝙇𝙊𝙎, 𝙊𝙉𝙇𝙔 𝙉𝙐𝙈𝘽𝙀𝙍𝙎 𝘼𝙉𝘿 𝙎𝙔𝙈𝘽𝙊𝙇𝙎 𝘼𝙍𝙀 𝘼𝙇𝙇𝙊𝙒𝙀𝘿 -, +, * , /, ×, ÷, π, e, (, ), !, ^, **, %`
         }
-
-        throw `${fg}𝙎𝙊𝙇𝙊 𝙎𝙀 𝘼𝘿𝙈𝙄𝙏𝙀𝙉 𝙉𝙐𝙈𝙀𝙍𝙊𝙎 𝙔 𝙎𝙄𝙈𝘽𝙊𝙇𝙊𝙎, 𝙊𝙉𝙇𝙔 𝙉𝙐𝙈𝘽𝙀𝙍𝙎 𝘼𝙉𝘿 𝙎𝙔𝙈𝘽𝙊𝙇𝙎 𝘼𝙍𝙀 𝘼𝙇𝙇𝙊𝙒𝙀𝘿 -, +, * , /, ×, ÷, π, e, (, ), !, ^, **`
     }
 }
-}
+
 function factorial(n) {
     n = parseInt(n);
     if (isNaN(n)) return 0;
@@ -56,5 +52,4 @@ function factorial(n) {
 handler.help = ['calc <expression>'];
 handler.tags = ['tools'];
 handler.command = /^(calc(ulat(e|or))?|kalk(ulator)?)$/i;
-handler.exp = 5;
-export default handler;
+export default handler
