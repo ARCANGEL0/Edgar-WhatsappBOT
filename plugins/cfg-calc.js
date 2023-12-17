004b7f3d25ffc068efc14a77252da95a2f6633ab
Henry <henry.arcangello@proton.me>
2023-12-17 13:55:53 +0000

    update plugins/cfg-calc.js

--- plugins/cfg-calc.js
+++ plugins/cfg-calc.js
@@ -5,6 +5,8 @@ let handler = async (m, { conn, text }) => {
     if (global.quiz.math) {
         m.reply('😨 𝙉𝙊 𝙃𝘼𝙂𝘼𝙎 𝙏𝙍𝘼𝙈𝙋𝘼!!\n𝘿𝙊 𝙉𝙊𝙏 𝘾𝙃𝙀𝘼𝙏!!');
     } else {
+      
+      console.log("test  "+6%15)
         let val = text
             .replace(/[^0-9\-\/+*×÷^πEe()%!.,]/g, '') // Adicionado vírgula (,) para reconhecer decimais
             .replace(/×/g, '*')
