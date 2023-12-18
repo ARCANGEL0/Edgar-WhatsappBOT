14c07ff642a9a87ea8aad8bf578cae790617a105
Henry <henry.arcangello@proton.me>
2023-12-18 08:02:48 +0000

    update plugins/cfg-calc.js

--- plugins/cfg-calc.js
+++ plugins/cfg-calc.js
@@ -11,7 +11,8 @@ let handler = async (m, { conn, text }) => {
 if (text.startsWith("lim ")) {
     try {
         // Extract the expression, value, and approaching from the input
-        let [ expression, value, approaching] = /lim (.*?)->(.*?) (.*)/.exec(text);
+       let [, expression, value, approaching] = /lim .*?->(.*?) (.*)/.exec(text);
+
 console.log("exp "+expression+"\nvalue "+value+"\n app "+ approaching)
         // Calculate the limit using the provided values
         let result = limit(value, approaching, expression);
