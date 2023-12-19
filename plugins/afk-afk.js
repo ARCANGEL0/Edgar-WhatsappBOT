5687ed7784fe9da95fe7aae3ffd5cf68d45925a0
Henry <henry.arcangello@proton.me>
2023-12-19 13:23:44 +0000

    update plugins/afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -4,8 +4,7 @@ let handler = async (m, { text, args, usedPrefix, command, conn }) => {
   if (!Array.isArray(user.afkChat)) {
   user.afkChat = []; // Initialize as an array if not already
 }
-console.log(m.chat)
-console.log('test  '+user.afkChat)
+
   // Check if it's a specific chat where AFK status needs to be set
 
 
