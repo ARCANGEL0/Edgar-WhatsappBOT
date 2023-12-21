2b28d13186038757b2cd27c6e0a1742c52ba74e8
Henry <henry.arcangello@proton.me>
2023-12-21 14:49:23 +0000

    update plugins/quizlogic.js

--- plugins/quizlogic.js
+++ plugins/quizlogic.js
@@ -5,7 +5,7 @@ handler.before = async function (m) {
     
     if(m.quoted){
       console.log("id  "+m.quoted.id)
-      console.log("texto  " m.quoted.text)
+      console.log("texto  "+ m.quoted.text)
     }
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
     global.quiz = global.quiz ? global.quiz : {}
