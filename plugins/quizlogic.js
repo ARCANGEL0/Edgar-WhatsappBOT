a4b19350afd309334e73094180759d9ec9c2053d
Henry <henry.arcangello@proton.me>
2023-12-21 14:44:18 +0000

    update plugins/quizlogic.js

--- plugins/quizlogic.js
+++ plugins/quizlogic.js
@@ -2,10 +2,15 @@
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
+    
+    if(m.quoted){
+      console.log(m.quoted)
+    }
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
     global.quiz = global.quiz ? global.quiz : {}
     if (!(id in global.quiz)) return m.reply('Ese juego ya ha terminado!')
     if (m.quoted.id == global.quiz[id].id) {
+      console.log("dentro do if:  "+ m.quoted.id)
         let resposta = global.quiz[m.chat].ca
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text == resposta ){
