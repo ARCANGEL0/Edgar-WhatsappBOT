017b12e9400d05f95133abddcdb7259c0d8d92e0
Henry <henry.arcangello@proton.me>
2023-12-21 14:23:50 +0000

    update plugins/quizlogic.js

--- plugins/quizlogic.js
+++ plugins/quizlogic.js
@@ -5,7 +5,7 @@ let id = m.chat
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
 console.log("inicio logica")
 
-if(m.quoted){
+if(m.quoted.sender){
   await conn.reply("teste")
 }
 
