33de480d10d23a8021d6271cc3dd085f16288b1d
Henry <henry.arcangello@proton.me>
2023-12-21 07:41:17 +0000

    update plugins/afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,4 +1,6 @@
-global.afks = global.afks ? global.afks : {}
+
+
+
 let handler = async (m, { text, args, usedPrefix, command, conn}) => { 
 let user = global.db.data.users[m.sender]
   
@@ -8,17 +10,9 @@ text = args.slice(0).join(" ")
 text = m.quoted.text
 } else return m.reply(`${lenguajeGB['smsAfkQ1'](usedPrefix, command)}`)
   
-if (text.length < 3) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
-// user.afk = + new Date
-
-global.afks[m.chat] ={
-  "usuario": m.sender,
-  
-  "razao" : text,
-  "tempo": new Date()
-}
-console.log(global.afks[m.chat])
-// user.afkReason = text
+if (text.length < 10) return m.reply(`${lenguajeGB['smsAfkQ2']()}`)
+user.afk = + new Date
+user.afkReason = text
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}*⬣━━━「 AFK ⚰️」━━━⬣*
     *🕯️💀𝕹𝖆𝖔 𝖕𝖊𝖗𝖙𝖚𝖗𝖇𝖊𝐞* *@${m.sender.split("@")[0]}* 
     *「 𝕴𝖗𝖆 𝖉𝖎𝖛𝖆𝖌𝖆𝖗 𝖕𝖊𝖑𝖆𝖘 𝖘𝖔𝖒𝖇𝖗𝖆𝖘 𝖉𝖔 𝖉𝖊𝖘𝖈𝖔𝖓𝖍𝖊𝖈𝖎𝖉𝖔, 𝖘𝖊 𝖆𝖚𝖘𝖊𝖓𝖙𝖆𝖓𝖉𝖔 𝖉𝖊𝖘𝖙𝖊 𝖛𝖆𝖑𝖊 𝖘𝖔𝖒𝖇𝖗𝖎𝖔.」*
