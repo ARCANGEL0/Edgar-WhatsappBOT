da6a85af78566a164df38acb94f55fcc10cae524
Henry <henry.arcangello@proton.me>
2023-12-22 19:21:18 +0000

    update plugins/afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -26,25 +26,9 @@ user.afkInfo[m.chat].afkTime = -1
 user.afkInfo[m.chat].afkReason = ''
 }
 else if(user.afkInfo[m.chat].afkTime < 0 || !user.afkInfo[m.chat]) {
-let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-for (let jid of jids) {
-let user = global.db.data.users[jid].afkInfo[m.chat]
-if (!user)
-continue
-let afkTime = user.afkTime
-if (!afkTime || afkTime < 0)
-continue
-let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}
-╭━━━━━━━━━⬣ 💀 ⬣━━━━━━━━━━━
 
-🕯️ ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '──┅❖  ' + reason : `${lenguajeGB['smsAfkM6']()}`}
 
-${lenguajeGB['smsAfkM3']()}\n──┅❖ *${(new Date - user.afk).toTimeString()}*
-
-╰━━━━━━━━━━━━━━━━━━⬣`.trim(), m);
-
-}
+await m.reply("test")
 
 }
 /*
