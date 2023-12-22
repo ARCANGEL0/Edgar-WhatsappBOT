f0308989b7cbf2ef38e4bbd6de5a4c3b41c325b4
Henry <henry.arcangello@proton.me>
2023-12-22 01:34:41 +0000

    update plugins/quiz.js

--- plugins/quiz.js
+++ plugins/quiz.js
@@ -13000,8 +13000,11 @@ else {
     
     }  
     else if(text.includes("placar") ) {
-      console.log(m.key)
+      console.log(m.key.participant)
+      console.log(m.key.id)
+      console.log(global.db.data.users[m.sender].silenced)
      let ps = groupMetadata.participants.map(v => v.id) // todos os membros
+     await conn.sendMessage(jid, { delete: m.key.id })
      await m.reply("placar")
       
       
