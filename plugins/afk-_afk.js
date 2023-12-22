15a70f21e64fc44feadcc8ee469a78954c670e86
Henry <henry.arcangello@proton.me>
2023-12-22 20:31:16 +0000

    update plugins/afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -47,7 +47,7 @@ if (
   m.quoted.sender &&
   global.db.data.chats[m.chat].chatafk.includes(m.quoted.sender || m.mentionedJid.toString().replace('@', '') + '@s.whatsapp.net')
 ) {
-  console.log(m.mentionedJid.toString().replace('@', '') + '@s.whatsapp.net')
+  
  let reason = global.db.data.users[m.quoted.sender].afkInfo[m.chat].afkReason ? global.db.data.users[m.quoted.sender].afkInfo[m.chat].afkReason : ''
 
 let time = global.db.data.users[m.quoted.sender].afkInfo[m.chat].afkTime
