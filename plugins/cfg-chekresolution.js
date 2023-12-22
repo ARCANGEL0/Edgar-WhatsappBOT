b3f0d49b1b95ce42905dee43d90af6c7a8df0606
Henry <henry.arcangello@proton.me>
2023-12-22 00:40:25 +0000

    update plugins/cfg-chekresolution.js

--- plugins/cfg-chekresolution.js
+++ plugins/cfg-chekresolution.js
@@ -1,30 +1,47 @@
-//By https://github.com/DIEGO-OFC
-
-import jimp from "jimp"
-import uploadImage from "../lib/uploadImage.js"
-import uploadFile from "../lib/uploadFile.js"
-
-let handler = async (m, { conn, usedPrefix }) => {
-	
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw "*[❕] POR FAVOR RESPONDE A UNA IMAGEN*"
-
-let media = await q.download()
-let isMedia = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-let link = await (isMedia ? uploadImage : uploadImage)(media)
-
-let source = await jimp.read(await link)
-let height = await source.getHeight()
-let width = await source.getWidth()
-
-m.reply(`_*RESOLUTION :*_ ${width} x ${height}
-> Ancho : ${width}
-> Altura : ${height}
-> Link: ${link}`)
+let handler = async (m, { conn, text, usedPrefix, command}) => {
+let user, number, bot, bant, ownerNumber, aa, users, usr, q, mime, img
+try {
+function no(number){
+return number.replace(/\s/g,'').replace(/([@+-])/g,'')}
+text = no(text)
+if(isNaN(text)) {
+number = text.split`@`[1]
+} else if(!isNaN(text)) {
+number = text
 }
-handler.help = ['cekresolution <reply | caption>', 'cekreso <reply | caption>']
-handler.tags = ['tool']
-handler.command = /^(cekreso(lution)?)$/i
-
+user = conn.user.jid.split`@`[0] + '@s.whatsapp.net'
+bot = conn.user.jid.split`@`[0] 
+bant = lenguajeGB.smsPropban1(usedPrefix, command, bot)
+if (!text && !m.quoted) return conn.reply(m.chat, bant, null, { mentions: [user] })               
+try {
+if(text) {
+user = number + '@s.whatsapp.net'
+} else if(m.quoted.sender) {
+user = m.quoted.sender
+} else if(m.mentionedJid) {
+user = number + '@s.whatsapp.net'
+}} catch (e) {
+} finally {
+number = user.split('@')[0]
+if(user === conn.user.jid) return conn.reply(m.chat, lenguajeGB.smsPropban2(bot), null, { mentions: [user] })   
+for (let i = 0; i < global.owner.length; i++) {
+ownerNumber = global.owner[i][0];
+if (user.replace(/@s\.whatsapp\.net$/, '') === ownerNumber) {
+aa = ownerNumber + '@s.whatsapp.net'
+await conn.reply(m.chat, lenguajeGB.smsPropban3(ownerNumber), null, { mentions: [aa] })
+return
+}}
+users = global.db.data.users
+if (users[user].silenced === true) conn.reply(m.chat, lenguajeGB.smsPropban4(number), null, { mentions: [user] }) 
+users[user].silenced = true
+usr = m.sender.split('@')[0]     
+await conn.reply(m.chat, lenguajeGB.smsPropban5(), null, { mentions: [user] })   
+//await conn.reply(user, lenguajeGB.smsPropban6(number, usr), null, { mentions: [user, m.sender] })
+}} catch (e) {
+await conn.reply(m.chat, lenguajeGB.smsPropban7(usedPrefix, command, number), null, m)
+console.log(e) 
+}}
+handler.command = /^mute|silenciar$/i
+handler.rowner = false
+handler.admin = true
 export default handler
