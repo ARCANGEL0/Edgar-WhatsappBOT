411677eb5dbf814bdb56c9f63678244969027bf0
Henry <henry.arcangello@proton.me>
2023-12-22 01:22:47 +0000

    update plugins/quiz.js

--- plugins/quiz.js
+++ plugins/quiz.js
@@ -13000,7 +13000,7 @@ else {
     
     }  
     else if(text.includes("placar") ) {
-      console.log(m)
+      console.log(m.sender)
      let ps = groupMetadata.participants.map(v => v.id) // todos os membros
      await m.reply("placar")
       
