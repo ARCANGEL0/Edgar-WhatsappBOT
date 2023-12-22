c66fd11dd7ee8164edb81a28285692c80b737a70
Henry <henry.arcangello@proton.me>
2023-12-22 10:52:19 +0000

    update handler.js

--- handler.js
+++ handler.js
@@ -338,7 +338,7 @@ return
 
 }
   
-  if (m.conversation && isOwner) {
+  if (m.text && isOwner) {
     console.log(m.message.conversation)
 console.log("teste 2")
 conn.sendMessage(m.chat, { delete: m.key })
