0b48a6261347c27bc0c866da9dc787ebbb5e0c9e
Henry <henry.arcangello@proton.me>
2023-12-23 14:36:15 +0000

    update plugins/quiz.js

--- plugins/quiz.js
+++ plugins/quiz.js
@@ -13043,7 +13043,7 @@ Object.entries(topjogadores[m.chat]).forEach(([group, players]) => {
     .sort(([, a], [, b]) => b.xp - a.xp) // Sort by XP level in descending order
     .slice(0, 5) // Take only the first 10 players
     .map(
-      ([number, { xp }]) => {
+      ([number, { xp,name }]) => {
         const userId = number.split('@')[0];
         mentionIds.push(number); // Add each number to the mentionIds array
         return `
