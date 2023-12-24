40dbfd2575cf881d4b5177e2dc370312b89ced31
Henry <henry.arcangello@proton.me>
2023-12-24 07:53:51 +0000

    update plugins/maker-simpcard.js

--- plugins/maker-simpcard.js
+++ plugins/maker-simpcard.js
@@ -1,10 +1,15 @@
 const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/simpcard', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*¡SIMP* 😆', m);
+  
+  imagem=  await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+  
+  
+  conn.sendFile(m.chat,imagem,"error.png","a",m)
+  
+  
+  
 };
-handler.help = ['simpcard'];
+handler.help = ['comunista'];
 handler.tags = ['maker'];
-handler.command = /^(simpcard)$/i;
+handler.command = /^(comunista)$/i;
 export default handler;
