135c1e397dfecf574291ebd957342b01ce9c4789
Henry <henry.arcangello@proton.me>
2023-12-24 11:32:42 +0000

    update plugins/maker-simpcard.js

--- plugins/maker-simpcard.js
+++ plugins/maker-simpcard.js
@@ -1,7 +1,7 @@
 const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   
-  imagem=  await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+  let imagem=  await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
   
   
   conn.sendFile(m.chat,imagem,"error.png","a",m)
