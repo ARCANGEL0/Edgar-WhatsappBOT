d4ced7658cd69848fb6688cf3522198d587d01ab
Henry <henry.arcangello@proton.me>
2023-12-24 19:18:22 +0000

    update plugins/maker-itssostupid.js

--- plugins/maker-itssostupid.js
+++ plugins/maker-itssostupid.js
@@ -2,9 +2,9 @@ const handler = async (m, {conn, args}) => {
   const text = args.slice(1).join(' ');
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, `https://api.popcat.xyz/communism?image=${
-    image: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')}, 'error.png',*@${who}*`, m);
+    image: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')}`, 'error.png',`𝘾𝙖𝙢𝙖𝙧𝙖𝙙𝙖 *@${who}*  ☭`, m);
 };
-handler.help = ['itssostupid', 'iss', 'stupid'];
+handler.help = ['commie', 'ussr', 'comunista'];
 handler.tags = ['maker'];
 handler.command = /^(itssostupid|iss|stupid)$/i;
 export default handler;
