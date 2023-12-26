5367a52e67ae8bbf1827deae495b33aab4f0e929
Henry <henry.arcangello@proton.me>
2023-12-26 23:43:44 +0000

    update plugins/chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 let name = conn.getName(m.sender)
 
 
-if (m.quoted && m.quoted.id === global.db.data.chats[m.chat].chatgpt["config"].lastQuestion ) {
+if (m.quoted && m.quoted.id /* global.db.data.chats[m.chat].chatgpt["config"].lastQuestion  */) {
 /* console.log(m.quoted.id)
   console.log(global.db.data.chats[m.chat].chatgpt["config"].lastQuestion)
   let newAiReply = requestToChatGPT(m.text)
