1fdb5ac995fe4c8e762059da4c0f82f49a172b7b
Henry <henry.arcangello@proton.me>
2023-12-26 23:32:46 +0000

    update plugins/maker-horncard.js

--- plugins/maker-horncard.js
+++ plugins/maker-horncard.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendMessage(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/comrade', {
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/comrade', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }),{mentions:who});
+  }),{mentions:who},`𝘾𝙖𝙢𝙖𝙧𝙖𝙙𝙖 *@${who.split("@")[0]}*  ☭`);
   console.log(who)
 };
 handler.help = ['comunista', 'ussr'];
