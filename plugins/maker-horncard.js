a05416bdf57af84a2f111491c50ba0c79d06dd56
Henry <henry.arcangello@proton.me>
2023-12-26 18:18:24 +0000

    update plugins/maker-horncard.js

--- plugins/maker-horncard.js
+++ plugins/maker-horncard.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn}) => {
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/horny', {
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/comrade', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'hornycard.png', '*TA PEGANDO FOGO. CHAMA OS BOMBEIROS🥵🔥*', m);
 };
