d8bcef5813347dea4e4439c23a1cb544ce02cc8c
Henry <henry.arcangello@proton.me>
2023-12-27 00:30:42 +0000

    update plugins/afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -34,11 +34,13 @@ console.log(m.sender+"  on set afk code")
   global.db.data.chats[m.chat] = {};
 }
 
-if (!global.db.data.chats[m.chat].chatafk) {
+if (!global.db.data.chats[m.chat].chatafk || !Array.isArray(global.db.data.chats[m.chat].chatafk)) {
   global.db.data.chats[m.chat].chatafk = [];
 }
-global.db.data.chats[m.chat].chatafk.push(m.sender)
-console.log(global.db.data.chats[m.chat].chatafk)
+
+global.db.data.chats[m.chat].chatafk.push(m.sender);
+console.log(global.db.data.chats[m.chat].chatafk);
+
 await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}*⬣━━━「 AFK ⚰️」━━━⬣*
     *🕯️💀𝕹𝖆𝖔 𝖕𝖊𝖗𝖙𝖚𝖗𝖇𝖊𝐞* *@${m.sender.split("@")[0]}* 
     *「 𝕴𝖗𝖆 𝖉𝖎𝖛𝖆𝖌𝖆𝖗 𝖕𝖊𝖑𝖆𝖘 𝖘𝖔𝖒𝖇𝖗𝖆𝖘 𝖉𝖔 𝖉𝖊𝖘𝖈𝖔𝖓𝖍𝖊𝖈𝖎𝖉𝖔, 𝖘𝖊 𝖆𝖚𝖘𝖊𝖓𝖙𝖆𝖓𝖉𝖔 𝖉𝖊𝖘𝖙𝖊 𝖛𝖆𝖑𝖊 𝖘𝖔𝖒𝖇𝖗𝖎𝖔.」*
