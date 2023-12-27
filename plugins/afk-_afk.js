0009d1c77a87dbfd6ac60d7c06130f814a5f1069
Henry <henry.arcangello@proton.me>
2023-12-27 23:01:17 +0000

    update plugins/afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -56,7 +56,7 @@ delete user.afkInfo[m.chat]
     m.mentionedJid
  //   && global.db.data.chats[m.chat].afks.includes(m.mentionedJid.toString())
 ) {
-  console.log(global.db.data.users[m.mentionedJid].afkInfo)
+  console.log(global.db.data.users[m.mentionedJid].afkInfo[m.mentionedJid])
   
   console.log(global.db.data.chats[m.chat].afks)
   
