cd33ece9aa22f9020e1f11e6273070660e49612e
Henry <henry.arcangello@proton.me>
2023-12-28 19:53:43 +0000

    update plugins/fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -8,22 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    if (data) {
-      const first10Entries = data.data.slice(0, 10);
-
-      for (const entry of first10Entries) {
-        const { abstract, fullText, ...rest } = entry;
-        console.log(rest); // Log the data except abstract and fullText
-        // You can send the data to the user or process it as needed
-      }
-    } else {
-      console.log('No data found.');
-      // Handle the case when no data is found
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    // Handle the error appropriately
-  }
+    console.log(data)
 };
 
 handler.command = /^core|pesquisar/i;
