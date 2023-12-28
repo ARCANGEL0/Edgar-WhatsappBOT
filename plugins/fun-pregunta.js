8f4e55ea25c5564c723cd50dc0366f24665bdf64
Henry <henry.arcangello@proton.me>
2023-12-28 19:42:29 +0000

    update plugins/fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -4,32 +4,20 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
  
  
  const apiKey = 'uaxjboqGrX63On79gMPc8zd5iKUtFYLI'
- const url = `https://api.core.ac.uk/v3/search/journal?apiKey=${apiKey}`;
+ 
    
 
-  const payload = {
-    q: text,
-    
-  };
+const apiUrl = `https://api.core.ac.uk/v3/search/works?q=${text}?apiKey=${apiKey}`;
+
 
   try {
-    const response = await fetch(url, {
-      method: 'POST', // or 'GET' depending on the API
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
+    const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data); // Process the response data as needed
+    console.log(data);
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error('Error fetching data:', error);
   }
+
 
 };
 
