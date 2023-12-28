b28d1d139c5d54dbaf3678181de842757c25ffd4
Henry <henry.arcangello@proton.me>
2023-12-28 19:24:42 +0000

    update plugins/fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
  const apiKey = 'uaxjboqGrX63On79gMPc8zd5iKUtFYLI'
 const searchQuery = text
 
-fetch(`https://api.core.ac.uk/v3/search/${searchQuery}?apiKey=${apiKey}`)
+fetch(`https://api.core.ac.uk/v3/search/fulltext/${searchQuery}?apiKey=${apiKey}`)
   .then(response => response.json())
   .then(data => {
     console.log(data)
