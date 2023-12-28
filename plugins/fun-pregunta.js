cc06c15c46792fbe6ff064fac4c6937fa0365609
Henry <henry.arcangello@proton.me>
2023-12-28 19:32:14 +0000

    update plugins/fun-pregunta.js

--- plugins/fun-pregunta.js
+++ plugins/fun-pregunta.js
@@ -4,14 +4,43 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
  
  
  const apiKey = 'uaxjboqGrX63On79gMPc8zd5iKUtFYLI'
-const searchQuery = text
+ const url = `https://api.core.ac.uk/v3/search/journal?apiKey=${apiKey}`;
+   
 
-fetch(`https://api.core.ac.uk/v3/search/fulltext/${searchQuery}?apiKey=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-  });
+  const payload = {
+    q: text,
+    scroll: true,
+    offset: 0,
+    limit: 0,
+    scroll_id: 'string',
+    entity_id: 'string',
+    entity_type: entityType,
+    stats: true,
+    raw_stats: true,
+    exclude: ['string'],
+    sort: ['string'],
+    accept: 'string',
+    measure: true,
+  };
 
+  try {
+    const response = await fetch(`${url}?entity_type=${entityType}`, {
+      method: 'POST', // or 'GET' depending on the API
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data); // Process the response data as needed
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 
 };
 
