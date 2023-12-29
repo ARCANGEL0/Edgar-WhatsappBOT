87ad512a90c30d7e60b08750128dc49b0e9026b4
Henry <henry.arcangello@proton.me>
2023-12-29 00:05:28 +0000

    update plugins/fun-frases.js

--- plugins/fun-frases.js
+++ plugins/fun-frases.js
@@ -5,5 +5,5 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
 let docname = text ? text : m.pushName || 'documento'
 conn.sendFile(m.chat, `${global.artigosLista[text]}`, docname + '.pdf', '', m, false, { asDocument: true })
 }
-handler.command = /^coredl/i
+handler.command = /^coredl|artigo/i
 export default handler
