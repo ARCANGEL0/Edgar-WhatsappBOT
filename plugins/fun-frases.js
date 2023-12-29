2f7d8529b4ddf8ff80260c79cf9ad5ace57fe612
Henry <henry.arcangello@proton.me>
2023-12-29 00:32:42 +0000

    update plugins/fun-frases.js

--- plugins/fun-frases.js
+++ plugins/fun-frases.js
@@ -1,9 +1,12 @@
 import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+// Function to get URL by index
 
-console.log(global.artigosLista.urls[text])
+
+
+console.log(global.artigosLista[0].urls[text])
 let docname = text ? text : m.pushName || 'documento'
-conn.sendFile(m.chat, `${global.artigosLista[text]}`, docname + '.pdf', '', m, false, { asDocument: true })
+conn.sendFile(m.chat, `${global.artigosLista[0].urls[text]}`, docname + '.pdf', '', m, false, { asDocument: true })
 }
 handler.command = /^coredl|artigo/i
 export default handler
