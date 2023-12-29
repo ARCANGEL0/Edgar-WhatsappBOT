ba548c80785db0f762f4b4a52f0fc472f448f6f5
Henry <henry.arcangello@proton.me>
2023-12-29 00:03:14 +0000

    update plugins/fun-frases.js

--- plugins/fun-frases.js
+++ plugins/fun-frases.js
@@ -1,8 +1,6 @@
 import uploadImage from '../lib/uploadImage.js'
 let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗔 𝗨𝗠𝗔 𝗜𝗠𝗔𝗚𝗘𝗠'    
+
 
 let docname = text ? text : m.pushName || 'documento'
 conn.sendFile(m.chat, `${global.artigosLista[text]}`, docname + '.pdf', '', m, false, { asDocument: true })
