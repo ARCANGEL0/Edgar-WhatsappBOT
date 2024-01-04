0bdaea3b8cf19eba7e5a5fef979b52dc7db01a25
Henry <henry.arcangello@proton.me>
2024-01-04 20:16:26 +0000

    update plugins/sticker-attp y ttp.js

--- plugins/sticker-attp y ttp.js
+++ plugins/sticker-attp y ttp.js
@@ -5,7 +5,7 @@ let kt = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 let teks = encodeURI(kt)
 
 if (command == 'attp') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+conn.sendFile(m.chat, `https://aemt.me/ttp?text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
     
 if (command == 'attp2') {
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m)}
