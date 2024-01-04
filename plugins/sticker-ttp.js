80266eb85a2b513b82588f1b55bad57976bbf512
Henry <henry.arcangello@proton.me>
2024-01-04 20:05:36 +0000

    update plugins/sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,7 +1,9 @@
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+
+let lls = await fetch(`https://aemt.me/ttp?text=${teks}`)
+let stiker = await sticker(null, lls, global.packname, global.author)
 if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, 
          
          
