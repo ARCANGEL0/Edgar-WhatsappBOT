455664c431f99ac7d15bcd5ad2c0a690de0f6233
Henry <henry.arcangello@proton.me>
2024-01-04 00:02:14 +0000

    update plugins/game-pista.js

--- plugins/game-pista.js
+++ plugins/game-pista.js
@@ -62,7 +62,7 @@ console.log(response)
         key: m.key }
     },
     m  )
-    conn.sendFile(m.chat, response.url, 'error.jpg', `*🥀 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 : ${text}*\n${wm}`, m)
+    conn.sendFile(m.chat, imageBlob, 'error.jpg', `*🥀 𝙍𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤 : ${text}*\n${wm}`, m)
   } catch (error) {
     console.error("Error while fetching Gen AI model API", error);
   }
