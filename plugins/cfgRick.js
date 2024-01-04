c12e52f65adf6980e66052247253e41280862330
Henry <henry.arcangello@proton.me>
2024-01-04 22:56:56 +0000

    update plugins/cfgRick.js

--- plugins/cfgRick.js
+++ plugins/cfgRick.js
@@ -141,7 +141,7 @@ await conn.sendMessage(m.chat, {react: {
         key: m.key}
     },
     m  )
-    let message = await  conn.sendFile(m.chat, rick.getRandom() to, 'rick.jpg', aiReply, m)
+    let message = await  conn.sendFile(m.chat, rick.getRandom(), 'rick.jpg', aiReply, m)
  
 global.db.data.chats[m.chat].rickgpt["config"].lastQuestion = message.key
  
