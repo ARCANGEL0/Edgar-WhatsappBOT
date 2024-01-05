c2a61bd3b6d17402fcfe56e18bdeb7229e08151d
Henry <henry.arcangello@proton.me>
2024-01-05 10:29:14 +0000

    update plugins/geupoPoll.js

--- plugins/geupoPoll.js
+++ plugins/geupoPoll.js
@@ -1,8 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
 	text = text.split(`|`)
-	if (!text || text.length == 1) throw `Format :\n*${usedPrefix + command} description|poll1|ppllh2|dst...*\n\nContoh :\n*${usedPrefix + command} romdom admin|iya|tidak*`
-	if (text.length > 1 && text.length < 3) throw `[!] Minimum inputs *2* selection!`
-	if (text.length > 13) throw `[!] Too many choices, max *12* !`
+	
 	let array = []
 	text.slice(1).forEach(function(i) { array.push(i) })
 	 conn.sendPoll(m.chat, "enquete", ["abc","dbfh"])
