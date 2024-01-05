5a55047eee82c8dc4b6b0b3495a2d08d252b9d7c
Henry <henry.arcangello@proton.me>
2024-01-05 12:14:49 +0000

    update plugins/geupoPoll.js

--- plugins/geupoPoll.js
+++ plugins/geupoPoll.js
@@ -1,13 +1,13 @@
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
-
+import translate from '@vitalets/google-translate-api' 
 let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
     let horoscopeUrl = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=';
     let horoArray = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"];
 console.log(text)
-  
+  let tstext = await translate(text, { to: "pt", autoCorrect: true })
         try {
-            let index = horoArray.indexOf(text.toLowerCase());
+            let index = horoArray.indexOf(tstext.toLowerCase());
             if (index === -1) {
                 await m.reply("Enter the right spelling");
             } else {
@@ -16,7 +16,13 @@ console.log(text)
                 const $ = cheerio.load(data);
                 const horoscope = $("body > div.grid.grid-right-sidebar.primis-rr > main > div.main-horoscope > p:nth-child(2)").text();
                 let result = `*Today's Date:-* ${horoscope.split("-")[0]}\n*Nature Hold's For You:-* ${horoscope.substring(horoscope.indexOf('-') + 1)}`;
-                await m.reply(result);
+                let nome = await translate(result, { to: "pt", autoCorrect: true })
+                await m.reply(`━━━━━━━━━⬣⛧༺♱༻⛧⬣━━━━━━━━
+${nome}
+━━━━━━━━━⬣𖤐⬣━━━━━━━━
+                `);
+                
+                
             }
         } catch (err) {
             console.log(err)
