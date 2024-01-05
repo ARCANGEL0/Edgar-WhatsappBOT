9d1e42f780a6c9b0563c69d0ca8da33e7279db6e
Henry <henry.arcangello@proton.me>
2024-01-05 23:17:07 +0000

    update plugins/cfgMath.js

--- plugins/cfgMath.js
+++ plugins/cfgMath.js
@@ -5,42 +5,32 @@ let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
  
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}❖─┅──┅ 𝗗𝗲𝘀𝗰𝗿𝗲𝘃𝗮-𝗺𝗲 𝗼 𝗴𝗲𝗻𝗲𝗿𝗼 𝗱𝗲 𝗳𝘂𝗻𝗱𝗼 𝗾𝘂𝗲 𝗽𝗿𝗼𝗰𝘂𝗿𝗮𝘀, 𝗰𝗼𝗺 𝗮𝘀 𝘀𝗼𝗺𝗯𝗿𝗮𝘀 𝘀𝘂𝘀𝘀𝘂𝗿𝗿𝗮𝗻𝘁𝗲𝘀 𝗱𝗲 𝘁𝘂𝗮 𝗽𝗿𝗲𝗳𝗲𝗿𝗲𝗻𝗰𝗶𝗮 𝗮 𝗱𝗮𝗻𝗰𝗮𝗿 𝗻𝗮𝘀 𝗽𝗮𝗿𝗲𝗱𝗲𝘀 𝗱𝗮 𝘀𝘂𝗮 𝗶𝗺𝗮𝗴𝗶𝗻𝗮𝗰𝗮𝗼 𝗹𝗮𝗺𝗲𝗻𝘁𝗮𝘃𝗲𝗹 🥀 ─┅──┅❖\n𝙀𝙓:\n*${usedPrefix + command} Cat*`
  
-const apiUrl = 'https://vihangayt.me/tools/mathssolve';
-
-
-const params = new URLSearchParams({
-  
-  q: text
-  
-});
-
-const url = `${apiUrl}?${params.toString()}`;
-
-fetch(url)
-.then(data =>{ 
- return data.json()
-})
-.then(buffer => {
-    console.log(buffer.data)
-    
-    let formula= buffer.data.toString()
-    
-  })
-  
-    // Assuming you have a function to create an Object URL from the blob
-  
-
  
-
+ const url = 'https://vihangayt.me/tools/mathssolve?q=';
  
-  .catch(error => console.error('Error:', error));
-  
-  
-let message = await translate(formula, { to: "pt", autoCorrect: true })
- )
-    await m.reply(`─┅──┅─┅──┅❖ 👁️ ❖─┅──┅─┅──┅
+         try {
+          m.react("🌒")
+            
+                const response = await fetch(url + `${text}`);
+                const data = await response.json();
+                let enmsg = data.data
+                let message = await translate(enmsg, { to: "pt", autoCorrect: true })
+
+                
+                console.log(message)
+                m.react("🌕")
+                
+await  m.reply(`─┅──┅─┅──┅❖ 👁️ ❖─┅──┅─┅──┅
    ⌬   ${message}   ⌬
 ─┅──┅──┅──┅❖❖─┅──┅─┅──┅─┅─`)
+                
+            }
+         catch (err) {
+            console.log(err)
+        }
+ 
+
+
 };
 
 handler.command = /^math|calc/i;
