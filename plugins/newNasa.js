b4500018cf80111fd22dd051e2f895e4944ff35b
Henry <henry.arcangello@proton.me>
2024-01-05 15:32:22 +0000

    update plugins/newNasa.js

--- plugins/newNasa.js
+++ plugins/newNasa.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text, usedPrefix, command, participants }) => {
             
                 
                 const response = await fetch(url);
-                console.log(response.json())
+                console.log(response
                 
        //         let message = await translate(enmsg, { to: "pt", autoCorrect: true })
                 
