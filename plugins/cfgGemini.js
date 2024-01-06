4739b728cea1d575b9e2be133ef277ad834679fb
Henry <henry.arcangello@proton.me>
2024-01-06 17:28:42 +0000

    update plugins/cfgGemini.js

--- plugins/cfgGemini.js
+++ plugins/cfgGemini.js
@@ -103,7 +103,7 @@ try {
 conn.sendPresenceUpdate('typing', m.chat);
 
  let msg = encodeURIComponent(text)
-    let res = await fetch(`https://vihangayt.me/tools/gemini?q=${text}`)
+    let res = await fetch(`https://vihangayt.me/tools/gemini?q=${prompt}`)
     console.log(res)
     let data = await res.json()
     console.log(data)
