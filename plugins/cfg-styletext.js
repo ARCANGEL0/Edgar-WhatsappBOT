a9f9492876826e4a584205cabdc2c5afc0e568d6
Henry <henry.arcangello@proton.me>
2024-01-07 22:33:39 +0000

    update plugins/cfg-styletext.js

--- plugins/cfg-styletext.js
+++ plugins/cfg-styletext.js
@@ -1,6 +1,8 @@
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 
+let stylenameConversions = {};
+
 let handler = async (m, { conn, args, isAdmin, isOwner }) => {
   if (!(isAdmin || isOwner) && global.db.data.chats[m.chat].jogos === false) {
     m.midia("â");
@@ -12,10 +14,14 @@ let handler = async (m, { conn, args, isAdmin, isOwner }) => {
     return conn.reply(m.chat, `Available styles:\n${allStyles.join(', ')}`, m);
   }
 
-  const stylename = args[0];
+  let stylename = args[0].toLowerCase();
+  if (stylenameConversions[stylename]) {
+    stylename = stylenameConversions[stylename].toLowerCase().replace(/ /g, '-');
+  }
+
   const text = args.slice(1).join(' ');
 
-  conn.reply(m.chat, `*${stylename}*\n${await stylizeText(stylename, text)}`, m);
+  conn.reply(m.chat, `*${stylename.replace(/-/g, ' ')}*\n${await stylizeText(stylename, text)}`, m);
 };
 
 handler.help = ['styletext'].map(v => v + ' (stylename) (text)');
@@ -35,30 +41,30 @@ async function stylizeText(stylename, text) {
   let obj = {};
 
   for (let tr of table) {
-    let name = tr.querySelector('.aname').innerHTML.toLowerCase();
+    let name = tr.querySelector('.aname').innerHTML.toLowerCase().replace(/ /g, '-');
     let content = tr.children[1].textContent.replace(/^\n/, '').replace(/\n$/, '');
     obj[name] = content;
   }
 
-  stylename = stylename.toLowerCase();
-  const stylizedText = obj[stylename] || obj[`${stylename} reversed`];
+  const stylizedText = obj[stylename];
 
   return stylizedText || 'Invalid stylename';
 }
 
 async function getAllStyles() {
-  let res = await fetch('http://qaz.wtf/u/convert.cgi?text=a');
-  let html = await res.text();
-  let dom = new JSDOM(html);
-  let table = dom.window.document.querySelector('table').children[0].children;
-  let styles = [];
+  if (Object.keys(stylenameConversions).length === 0) {
+    let res = await fetch('http://qaz.wtf/u/convert.cgi?text=a');
+    let html = await res.text();
+    let dom = new JSDOM(html);
+    let table = dom.window.document.querySelector('table').children[0].children;
 
-  for (let tr of table) {
-    let name = tr.querySelector('.aname').innerHTML;
-    styles.push(name);
+    for (let tr of table) {
+      let name = tr.querySelector('.aname').innerHTML.toLowerCase().replace(/ /g, '-');
+      stylenameConversions[name] = name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
   }
 
-  return styles;
+  return Object.keys(stylenameConversions);
 }
 
 export default handler;
