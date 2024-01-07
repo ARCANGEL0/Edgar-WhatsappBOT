ff0c0e23de3137ef2866e4d99043cbbfd28ec94f
Henry <henry.arcangello@proton.me>
2024-01-07 11:42:23 +0000

    update plugins/dono-userdata.js

--- plugins/dono-userdata.js
+++ plugins/dono-userdata.js
@@ -1,12 +1,16 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn,text,command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-
+if(command=="userdata"){
 console.log(global.db.data.users[who])
 conn.reply(m.chat, "---Log enviado 🕯️---")
-
+}
+if(command=="chdata"){
+console.log(global.db.data.chats[m.chat])
+conn.reply(m.chat, "---Log enviado 🕯️---")
+}
 };
 handler.help = ['gay']
 handler.tags = ['maker']
-handler.command = /^(userdata)$/i
+handler.command = /^(userdata|chdat)$/i
 handler.owner= true
 export default handler
