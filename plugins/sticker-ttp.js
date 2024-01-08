847975b94956039da1859a8f6589615d0457cc72
Henry <henry.arcangello@proton.me>
2024-01-08 10:48:06 +0000

    update plugins/sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,17 +1,33 @@
-import { sticker } from '../lib/sticker.js'
-let handler = async (m, { conn, text }) => {
-let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 
-let lls = await fetch(`https://aemt.me/ttp?text=${teks}`)
-console.log(lls.buffer())
-let stiker = await sticker(lls.blob,lls.blob, global.packname, global.author)
-if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, 
-         
-         
-       }}, { quoted: m })
-throw stiker.toString()
+import { sticker } from '../lib/sticker.js'
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+
+
+let kt = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+let teks = encodeURI(kt)
+let stiker = await sticker(false,`https://aemt.me/ttp?text=${teks}`, global.packname, global.author)
+if (command == 'attp') {
+conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, { asSticker: true })}
+    
+if (command == 'attp2') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m)}
+
+if (command == 'ttp5') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+    
+if (command == 'ttp4') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+    
+if (command == 'ttp3') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+    
+if (command == 'ttp2') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+    
+if (command == 'ttp') {
+conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+    
 }
-handler.help = ['ttp <teks>']
+handler.command = handler.help = ['ttp', 'ttp2', 'ttp3', 'ttp4', 'ttp5', 'attp', 'attp2']
 handler.tags = ['sticker']
-handler.command = /^ttpg$/i 
 export default handler
