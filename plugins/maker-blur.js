d2807353d95f252410a4b6941462f31b2df730d3
Henry <henry.arcangello@proton.me>
2024-01-08 10:04:44 +0000

    update plugins/maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -1,4 +1,9 @@
-const handler = async (m, {conn, usedprefix}) => {
+const handler = async (m, {conn, usedprefix, isAdmin,isOwner}) => {
+  if(!(isAdmin || isOwner) && global.db.data.chats[m.chat].midia===false){
+   m.react("❌")
+   
+   return !0;
+ } 
   const who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/blur', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
@@ -7,4 +12,5 @@ const handler = async (m, {conn, usedprefix}) => {
 handler.help = ['blur', 'difuminar2'];
 handler.tags = ['maker'];
 handler.command = /^(blur|difuminar2)$/i;
+handler.admin = true
 export default handler;
