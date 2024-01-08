394a9fd6fefc1efe9403580041ae7e0b27de4fdd
Henry <henry.arcangello@proton.me>
2024-01-08 00:20:49 +0000

    update plugins/dl-wallpaper.js

--- plugins/dl-wallpaper.js
+++ plugins/dl-wallpaper.js
@@ -1,7 +1,11 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
- 
+let handler = async (m, { conn, text, usedPrefix, command, isOwner,isAdmin }) => {
+ if(!(isAdmin || isOwner) && global.db.data.chats[m.chat].busca===false){
+   m.react("❌")
+   
+   return !0;
+ } 
  
 if (!text) throw `${lenguajeGB['smsAvisoMG']()}❖─┅──┅ 𝗗𝗲𝘀𝗰𝗿𝗲𝘃𝗮-𝗺𝗲 𝗼 𝗴𝗲𝗻𝗲𝗿𝗼 𝗱𝗲 𝗳𝘂𝗻𝗱𝗼 𝗾𝘂𝗲 𝗽𝗿𝗼𝗰𝘂𝗿𝗮𝘀, 𝗰𝗼𝗺 𝗮𝘀 𝘀𝗼𝗺𝗯𝗿𝗮𝘀 𝘀𝘂𝘀𝘀𝘂𝗿𝗿𝗮𝗻𝘁𝗲𝘀 𝗱𝗲 𝘁𝘂𝗮 𝗽𝗿𝗲𝗳𝗲𝗿𝗲𝗻𝗰𝗶𝗮 𝗮 𝗱𝗮𝗻𝗰𝗮𝗿 𝗻𝗮𝘀 𝗽𝗮𝗿𝗲𝗱𝗲𝘀 𝗱𝗮 𝘀𝘂𝗮 𝗶𝗺𝗮𝗴𝗶𝗻𝗮𝗰𝗮𝗼 𝗹𝗮𝗺𝗲𝗻𝘁𝗮𝘃𝗲𝗹 🥀 ─┅──┅❖\n𝙀𝙓:\n*${usedPrefix + command} Cat*`
  
