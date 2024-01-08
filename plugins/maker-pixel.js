ad7553f2051a6b4ca05afce7f87c5517c09e7ea8
Henry <henry.arcangello@proton.me>
2024-01-08 12:20:19 +0000

    update plugins/maker-pixel.js

--- plugins/maker-pixel.js
+++ plugins/maker-pixel.js
@@ -10,8 +10,7 @@ conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/pixelate
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 comment: text,
 username: conn.getName(m.sender)
-}), 'error.png', '𓄿 *𝓔𝓭𝓰𝓪𝓻 𝓐𝓵𝓵𝓪𝓷 𝓑𝓸𝓽 🐈‍⬛
-*', m)
+}), 'error.png', '𓄿 *𝓔𝓭𝓰𝓪𝓻 𝓐𝓵𝓵𝓪𝓷 𝓑𝓸𝓽🐈‍⬛*', m)
 }
 handler.help = ['pixel','difuminar']
 handler.tags = ['maker']
