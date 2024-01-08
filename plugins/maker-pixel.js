3853734c07a873f4a033e174378c3a48db8b3a30
Henry <henry.arcangello@proton.me>
2024-01-08 10:06:46 +0000

    update plugins/maker-pixel.js

--- plugins/maker-pixel.js
+++ plugins/maker-pixel.js
@@ -1,10 +1,17 @@
-let handler = async (m, { conn, usedprefix, text }) => {
+let handler = async (m, { conn, usedprefix, text,isAdmin,isOwner}) => {
+  
+  if(!(isAdmin || isOwner) && global.db.data.chats[m.chat].midia===false){
+   m.react("❌")
+   
+   return !0;
+ } 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/pixelate', {
 avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
 comment: text,
 username: conn.getName(m.sender)
-}), 'error.png', '🌀 *AQUÍ ESTA LA IMAGEN PIXELADA!!*\n𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨', m)
+}), 'error.png', '𓄿 *𝓔𝓭𝓰𝓪𝓻 𝓐𝓵𝓵𝓪𝓷 𝓑𝓸𝓽 🐈‍⬛
+*', m)
 }
 handler.help = ['pixel','difuminar']
 handler.tags = ['maker']
