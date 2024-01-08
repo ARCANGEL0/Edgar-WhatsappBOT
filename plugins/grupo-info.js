0d3d4ca345598df1b9b8e4c374bab17b28fd7851
Henry <henry.arcangello@proton.me>
2024-01-08 12:31:18 +0000

    update plugins/grupo-info.js

--- plugins/grupo-info.js
+++ plugins/grupo-info.js
@@ -4,7 +4,7 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let text = 
-`─┅──┅❖ ${lenguajeGB['smsGI1']()} ❖─┅──┅
+`─┅❖ ${lenguajeGB['smsGI1']()} 
 
 𓂀 ${lenguajeGB['smsGI2']()}
  ⏦ ${groupMetadata.id}
