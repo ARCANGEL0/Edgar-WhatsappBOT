7292ee67f0c75e56eb3149380937ce74f286b2bf
Henry <henry.arcangello@proton.me>
2024-01-09 07:52:35 +0000

    update plugins/conversor-audio.js

---
+++ plugins/conversor-audio.js
@@ -0,0 +1,15 @@
+
+let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw '𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗔 𝗨𝗠𝗔 𝗜𝗠𝗔𝗚𝗘𝗠'    
+
+audio = args[0]
+			
+			txt = await fetch(`https://api.lolhuman.xyz/api/speech-to-text?apikey=${apikey}&audio=${audio}`)
+			
+			console.log(txt.json())
+
+}
+handler.command = /^text/i
+export default handler
