bff7fa18474655982220247b9bc2bf340b5d4427
Henry <henry.arcangello@proton.me>
2024-01-09 10:58:03 +0000

    update config.js

--- config.js
+++ config.js
@@ -8,7 +8,8 @@ import axios from "axios"
 import moment from "moment-timezone"
 import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 
-require('dotenv').config();
+import('dotenv').then(dotenv => dotenv.config());
+
 
 const apiKey = process.env.OPENAI_API_KEY;
 console.log("api open  " + apiKey)
